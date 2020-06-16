cee9ccb4be121c9e9a99bdeaa2cbdae5a29d1414
Jessé Sampaio <jessesampaio.ti@gmail.com>
2020-06-16 15:56:26 -0300

    Implementação método criar categoria
    Implementação dos métodos criar e buscar categoria.
    
    O segundo método retorna status 404 caso a categoria informado for nula.

--- src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
@@ -1,11 +1,21 @@
 package com.algaworks.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.repository.CategoriaRepository;
@@ -20,7 +30,7 @@ public class CategoriaResource {
 	
 	/**
 	 * Tipo: GET
-	 * Parâmetros: vazio 
+	 * Parâmetros: Vazio 
 	 * Endereço: http://localhost:8080/categorias
 	 *  
 	 */
@@ -29,6 +39,34 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 	
+	/**
+	 * Tipo: POST
+	 * Parâmetros: { "nome" : "Impostos" } 
+	 * Endereço: http://localhost:8080/categorias
+	 *  
+	 */
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);	
 	
+	}
+	
+	/**
+	 * Tipo: GET
+	 * Parâmetros: {codigo} 
+	 * Endereço: http://localhost:8080/categorias/{codigo}
+	 *  
+	 */
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		 Categoria categoria = categoriaRepository.findOne(codigo);
+		 return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+	}
 	
 }
