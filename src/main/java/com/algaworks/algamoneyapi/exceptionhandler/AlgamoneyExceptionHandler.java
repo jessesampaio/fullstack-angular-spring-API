00d33a85b00c5a6248e3e6d88858d3f6b74124bc
Jessé Sampaio <jessesampaio.ti@gmail.com>
2020-07-14 13:53:07 -0300

    5.4 Validando inconsistências
    Validando inconsistências

--- src/main/java/com/algaworks/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.xml.ws.BindingType;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,7 +20,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -35,18 +35,15 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.getCause() != null ? ex.getCause().toString() : ex.toString();
-		
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-		
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
-			
 	}
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
-		List<Erro> erros = criarListaDeErros(ex.getBindingResult());		
+		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
 	
@@ -55,25 +52,31 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.toString();
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-		
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
-	private List<Erro> criarListaDeErros(BindingResult bindingResult){
+	@ExceptionHandler({ DataIntegrityViolationException.class } )
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
 		List<Erro> erros = new ArrayList<>();
 		
-		for(FieldError fieldError : bindingResult.getFieldErrors()) {
-			
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
 			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
 			String mensagemDesenvolvedor = fieldError.toString();
-			erros.add(new Erro(mensagemUsuario,mensagemDesenvolvedor));
-			
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		}
-		
+			
 		return erros;
 	}
 	
-	public static class Erro{
+	public static class Erro {
+		
 		private String mensagemUsuario;
 		private String mensagemDesenvolvedor;
 		
@@ -86,18 +89,11 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			return mensagemUsuario;
 		}
 
-		public void setMensagemUsuario(String mensagemUsuario) {
-			this.mensagemUsuario = mensagemUsuario;
-		}
-
 		public String getMensagemDesenvolvedor() {
 			return mensagemDesenvolvedor;
-		}
-
-		public void setMensagemDesenvolvedor(String mensagemDesenvolvedor) {
-			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
 		}
 		
 	}
 	
 }
+
