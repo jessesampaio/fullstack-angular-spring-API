2a5e91843acacad4fb780ad2b7857f2f1cc42f05
Jessé Sampaio <jessesampaio.ti@gmail.com>
2020-07-14 16:05:19 -0300

    5.6. Regra para não salvar pessoa inativa
    Regra para não salvar Pessoa inativa e melhoria nas mensagens

--- src/main/java/com/algaworks/algamoneyapi/service/PessoaService.java
+++ src/main/java/com/algaworks/algamoneyapi/service/PessoaService.java
@@ -8,42 +8,32 @@ import org.springframework.stereotype.Service;
 import com.algaworks.algamoneyapi.model.Pessoa;
 import com.algaworks.algamoneyapi.repository.PessoaRepository;
 
+
 @Service
 public class PessoaService {
-
+	
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
-	Pessoa pessoaSalva;
-	
+
 	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
-
-		pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
 		
-		//copia pessoa para pessoa salva ignorando o atributo código
 		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
-		
 		return pessoaRepository.save(pessoaSalva);
-		
 	}
-
 
 	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
-		pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
 		pessoaSalva.setAtivo(ativo);
 		pessoaRepository.save(pessoaSalva);
-		
 	}
-
-	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
-		
+	
+	public Pessoa buscarPessoaPeloCodigo(Long codigo) {
 		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
-		
-		if(pessoaSalva ==  null) {
+		if (pessoaSalva == null) {
 			throw new EmptyResultDataAccessException(1);
 		}
-		
 		return pessoaSalva;
-		
 	}
+	
 }
