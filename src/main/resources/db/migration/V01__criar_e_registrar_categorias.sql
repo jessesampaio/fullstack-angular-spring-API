e512a358099fb3c7d0f0e42483a073fb3230ebc1
Jessé Sampaio <jessesampaio.ti@gmail.com>
2020-07-21 16:01:37 -0300

    6.5 Configurando JWT no projeto
    Implementando a configuração JWT no projeto

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,8 +1,7 @@
-CREATE TABLE categoria(
+CREATE TABLE categoria (
 	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 INSERT INTO categoria (nome) values ('Lazer');
 INSERT INTO categoria (nome) values ('Alimentação');
